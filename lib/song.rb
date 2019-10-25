ab597cd912f4de24b95fa02fcc3a4343c3625b97
Karl Benbeneck <kbenbeneck@gmail.com>
2019-10-25 06:57:34 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -15,9 +15,14 @@ class Song
   def self.new_by_filename(filename)
     artist, song = filename.split(" - ")
     new_song = self.new(song)
-    new_song.save
+    new_song.
   end
 
+  def artist_name=(name)
+    self.artist = Artist.find_or_create_by_name(name)
+    artist.add_song(self)
+  end
+  
   def save
     @@all << self
   end
